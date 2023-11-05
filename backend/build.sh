2220191a3a628746fb231b2a7363cd40420f410c
Forb.Jok <forbjok@proton.me>
2023-11-05 22:26:56 +0100

    fix: Update to use pnpm

--- backend/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export SQLX_OFFLINE=true
-
-# Build
-cargo build --release
-
-mkdir -p /blazebooru
-cp ./target/x86_64-unknown-linux-musl/release/blazebooru /blazebooru/
--- http/build-frontend.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /blazebooru/source/frontend
-
-npm install
-npm run build-only
